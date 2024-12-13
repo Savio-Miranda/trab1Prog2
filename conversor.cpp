74c9383db83c438081bf2b7ee19a75e706d99ba5
luiz2801 <luizmoreira@duck.com>
2024-12-12 22:39:25 -0300

    Conversor de moeda versão final

--- conversor.cpp
+++ conversor.cpp
@@ -3,64 +3,63 @@ using namespace std;
 
 class moeda {
 private:
-    float valor;
 
-    float realDolar() {
-        return valor / 6;
+    float realDolar(float valor) {
+        return valor / 6.09;
     }
 
-    float realEuro() {
+    float realEuro(float valor) {
         return valor / 6.33;
     }
 
-    float realLibra() {
+    float realLibra(float valor) {
         return valor / 7.66;
     }
 
-    float dolarReal() {
-        return valor / 0.17;
+    float dolarReal(float valor) {
+        return valor / 0.166;
     }
 
-    float dolarLibra() {
+    float dolarLibra(float valor) {
         return valor / 1.27;
     }
 
-    float dolarEuro() {
+    float dolarEuro(float valor) {
         return valor / 1.05;
     }
 
-    float euroReal() {
+    float euroReal(float valor) {
         return valor / 0.16;
     }
 
-    float euroDolar() {
+    float euroDolar(float valor) {
         return valor / 0.95;
     }
 
-    float euroLibra() {
+    float euroLibra(float valor) {
         return valor / 1.21;
     }
 
-    float libraReal() {
+    float libraReal(float valor) {
         return valor / 0.13;
     }
 
-    float libraDolar() {
+    float libraDolar(float valor) {
         return valor / 0.79;
     }
 
-    float libraEuro() {
+    float libraEuro(float valor) {
         return valor / 0.83;
     }
 
 public:
     // real, dólar, euro e libra
-    float valor;
+    float qte;
     int moeda;
 
     void converter() {
         cout << "Quanto você tem?" << endl;
-        cin >> valor;
+        cin >> qte;
 
         cout << "Qual moeda você tem? " << endl;
         cout << "1-Real" << endl;
@@ -79,11 +78,11 @@ public:
 
             cin >> convertida;
             if (convertida == 1) {
-                cout << "Você receberá " << realDolar() << endl;
+                cout << "Você receberá " << realDolar(qte) << endl;
             } else if (convertida == 2) {
-                cout << "Você receberá " << realLibra() << endl;
+                cout << "Você receberá " << realLibra(qte) << endl;
             } else {
-                cout << "Você receberá " << realEuro() << endl;
+                cout << "Você receberá " << realEuro(qte) << endl;
             }
             break;
 
@@ -95,11 +94,11 @@ public:
 
             cin >> convertida;
             if (convertida == 1) {
-                cout << "Você receberá " << dolarReal() << endl;
+                cout << "Você receberá " << dolarReal(qte) << endl;
             } else if (convertida == 2) {
-                cout << "Você receberá " << dolarLibra() << endl;
+                cout << "Você receberá " << dolarLibra(qte) << endl;
             } else {
-                cout << "Você receberá " << dolarEuro() << endl;
+                cout << "Você receberá " << dolarEuro(qte) << endl;
             }
             break;
 
@@ -111,11 +110,11 @@ public:
 
             cin >> convertida;
             if (convertida == 1) {
-                cout << "Você receberá " << euroReal() << endl;
+                cout << "Você receberá " << euroReal(qte) << endl;
             } else if (convertida == 2) {
-                cout << "Você receberá " << euroLibra() << endl;
+                cout << "Você receberá " << euroLibra(qte) << endl;
             } else {
-                cout << "Você receberá " << euroDolar() << endl;
+                cout << "Você receberá " << euroDolar(qte) << endl;
             }
             break;
 
@@ -127,11 +126,11 @@ public:
 
             cin >> convertida;
             if (convertida == 1) {
-                cout << "Você receberá " << libraReal() << endl;
+                cout << "Você receberá " << libraReal(qte) << endl;
             } else if (convertida == 2) {
-                cout << "Você receberá " << libraDolar() << endl;
+                cout << "Você receberá " << libraDolar(qte) << endl;
             } else {
-                cout << "Você receberá " << libraEuro() << endl;
+                cout << "Você receberá " << libraEuro(qte) << endl;
             }
             break;
         }
